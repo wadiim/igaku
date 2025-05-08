99901f41df07bf3e44848360713e3c8bef0ab5f9
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2025-05-09 00:37:17 +0200

    Simplify inter-service integration tests

--- auth-service/tests/user_client_integration_test.go
+++ auth-service/tests/user_client_integration_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"context"
-	"fmt"
 	"net/http"
 	"strings"
 	"testing"
@@ -20,63 +19,36 @@ import (
 
 func TestUserClient(t *testing.T) {
 	t.Run("TestSetup", func(t *testing.T) {
-		t.Parallel()
 		ctx := context.Background()
+		cleanup := testUtils.SetupTestServices(ctx, t)
+		defer cleanup()
+		url := "http://localhost:8080/hello"
 
-		netName := "test-setup"
-		network, netCleanup := testUtils.SetupTestNetwork(ctx, t, netName)
-		defer netCleanup()
-
-		db, dbCleanup := testUtils.SetupTestDatabase(ctx, t, network, netName)
-		defer dbCleanup()
-
-		client, clientCleanup := testUtils.SetupTestUserService(ctx, t, db, network, netName)
-		defer clientCleanup()
-
-		url := fmt.Sprintf("http://%s:%s/hello", client.Host, client.Port)
 		res, err := http.Get(url)
 		require.NoError(t, err)
 		require.Equal(t, http.StatusOK, res.StatusCode)
 	})
 
 	t.Run("TestFindByUsername_NotFound", func(t *testing.T) {
-		t.Parallel()
 		ctx := context.Background()
+		cleanup := testUtils.SetupTestServices(ctx, t)
+		url := "http://localhost:8080"
 
-		netName := "findbyusername-notfound"
-		network, netCleanup := testUtils.SetupTestNetwork(ctx, t, netName)
-		defer netCleanup()
-
-		db, dbCleanup := testUtils.SetupTestDatabase(ctx, t, network, netName)
-		defer dbCleanup()
-
-		client, clientCleanup := testUtils.SetupTestUserService(ctx, t, db, network, netName)
-		defer clientCleanup()
-
-		url := fmt.Sprintf("http://%s:%s", client.Host, client.Port)
 		userClient := clients.NewUserClient(url)
-
 		user, err := userClient.FindByUsername("nonexistinguser")
 		assert.Nil(t, user, "Expected no user to be returned")
 		require.NotNil(t, err, "Expected error when finding non-existend user")
 		assert.Contains(t, strings.ToLower(err.Error()), "user not found")
+
+		cleanup()
 	})
 
 	t.Run("TestFindByUsername_Success", func(t *testing.T) {
-		t.Parallel()
 		ctx := context.Background()
+		cleanup := testUtils.SetupTestServices(ctx, t)
+		defer cleanup()
+		url := "http://localhost:8080"
 
-		netName := "findbyusername-success"
-		network, netCleanup := testUtils.SetupTestNetwork(ctx, t, netName)
-		defer netCleanup()
-
-		db, dbCleanup := testUtils.SetupTestDatabase(ctx, t, network, netName)
-		defer dbCleanup()
-
-		client, clientCleanup := testUtils.SetupTestUserService(ctx, t, db, network, netName)
-		defer clientCleanup()
-
-		url := fmt.Sprintf("http://%s:%s", client.Host, client.Port)
 		userClient := clients.NewUserClient(url)
 
 		user, err := userClient.FindByUsername("jdoe")
@@ -104,20 +76,11 @@ func TestUserClient(t *testing.T) {
 	})
 
 	t.Run("TestPersist_Success", func(t *testing.T) {
-		t.Parallel()
 		ctx := context.Background()
+		cleanup := testUtils.SetupTestServices(ctx, t)
+		defer cleanup()
+		url := "http://localhost:8080"
 
-		netName := "persist-success"
-		network, netCleanup := testUtils.SetupTestNetwork(ctx, t, netName)
-		defer netCleanup()
-
-		db, dbCleanup := testUtils.SetupTestDatabase(ctx, t, network, netName)
-		defer dbCleanup()
-
-		client, clientCleanup := testUtils.SetupTestUserService(ctx, t, db, network, netName)
-		defer clientCleanup()
-
-		url := fmt.Sprintf("http://%s:%s", client.Host, client.Port)
 		userClient := clients.NewUserClient(url)
 
 		id, err := uuid.Parse("263bd7aa-46d1-4253-9232-fba5d68e161c")
@@ -155,5 +118,3 @@ func TestUserClient(t *testing.T) {
 		)
 	})
 }
-
-// TODO: Reduce code duplication
--- auth-service/tests/utils/utils.go
+++ auth-service/tests/utils/utils.go
@@ -1,150 +1,52 @@
 package utils
 
 import (
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go/wait"
-	tc "github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/compose"
 
 	"context"
 	"testing"
 	"time"
 )
 
-type TestDatabase struct {
-	Host		string
-	Port		string
-	DBName		string
-	User		string
-	Password	string
-}
-
-type TestUserService struct {
-	Host	string
-	Port	string
-}
-
-func SetupTestNetwork(
-	ctx context.Context, t *testing.T, netName string,
-) (network tc.Network, cleanup func()) {
-	t.Helper()
-
-	network, err := tc.GenericNetwork(ctx, tc.GenericNetworkRequest{
-		NetworkRequest: tc.NetworkRequest{
-			Name:		netName,
-			CheckDuplicate:	true,
-		},
-	})
-	require.NoError(t, err, "Failed to create a network")
-
-	cleanup = func() {
-		if err := network.Remove(ctx); err != nil {
-			t.Errorf("Failed to remove test network: %v", err)
-		}
-	}
-
-	return network, cleanup
-}
-
-func SetupTestDatabase(
+func SetupTestServices(
 	ctx context.Context,
 	t *testing.T,
-	network tc.Network,
-	netName string,
-) (db *TestDatabase, cleanup func()) {
-	t.Helper()
+) (cleanup func()) {
+	composeFilePath := "../../compose.yaml"
 
-	host := "postgres"
-	dbName := "testdb"
-	user := "testuser"
-	password := "testpass"
-
-	req := tc.ContainerRequest{
-		Image: "postgres:15",
-		ExposedPorts: []string{"5432/tcp"},
-		Env: map[string]string{
-			"POSTGRES_DB": dbName,
-			"POSTGRES_USER": user,
-			"POSTGRES_PASSWORD": password,
-		},
-		WaitingFor: wait.ForLog("database system is ready to accept connections").
-			WithOccurrence(2).WithStartupTimeout(10*time.Second),
-		Networks: []string{netName},
-		NetworkAliases: map[string][]string{netName: {host}},
-	}
-
-	container, err := tc.GenericContainer(ctx, tc.GenericContainerRequest{
-		ContainerRequest: req,
-		Started: true,
-	})
-	require.NoError(t, err, "Failed to start PostgreSQL container")
-
-	cleanup = func() {
-		if err := container.Terminate(ctx); err != nil {
-			t.Errorf("Failed to terminate PostgreSQL container: %v", err)
-		}
-	}
-
-	testDatabase := TestDatabase{
-		Host: host,
-		Port: "5432",
-		DBName: dbName,
-		User: user,
-		Password: password,
-	}
-
-	return &testDatabase, cleanup
-}
-
-func SetupTestUserService(
-	ctx context.Context,
-	t *testing.T,
-	db *TestDatabase,
-	network tc.Network,
-	netName string,
-) (client *TestUserService, cleanup func()) {
-	t.Helper()
+	stack, err := compose.NewDockerCompose(composeFilePath)
+	require.NoError(t, err, "Failed to create stack")
 
 	env := map[string]string{
-		"POSTGRES_HOST":	db.Host,
-		"POSTGRES_PORT":	db.Port,
-		"POSTGRES_DB":		db.DBName,
-		"POSTGRES_USER":	db.User,
-		"POSTGRES_PASSWORD":	db.Password,
+		"POSTGRES_DB":		"igakudb",
+		"POSTGRES_USER":	"igaku",
 	}
 
-	req := tc.ContainerRequest{
-		FromDockerfile: tc.FromDockerfile{
-			Context:	"../../",
-			Dockerfile:	"Dockerfile",
-		},
-		ExposedPorts:	[]string{"8080/tcp"},
-		Env:		env,
-		Networks:	[]string{netName},
-		WaitingFor:	wait.ForHTTP("/hello").WithStartupTimeout(30*time.Second),
-	}
-
-	container, err := tc.GenericContainer(ctx, tc.GenericContainerRequest{
-		ContainerRequest:	req,
-		Started:		true,
-	})
-	require.NoError(t, err, "Failed to start user service container")
+	err = stack.
+		WithEnv(env).
+		WaitForService("db", wait.ForHealthCheck()).
+		WaitForService(
+			"api",
+			wait.NewHTTPStrategy("/hello").
+				WithPort("8080/tcp").
+				WithStartupTimeout(180*time.Second).
+				WithPollInterval(2*time.Second),
+		).
+		Up(ctx, compose.Wait(true))
+	require.NoError(t, err, "Failed to start stack")
 
 	cleanup = func() {
-		if err := container.Terminate(ctx); err != nil {
-			t.Errorf("Failed to terminate user service container: %v", err)
-		}
+		err = stack.Down(
+			context.Background(),
+			compose.RemoveOrphans(true),
+			compose.RemoveVolumes(true),
+			compose.RemoveImagesLocal,
+		)
+		assert.NoError(t, err, "Failed to stop stack")
 	}
 
-	host, err := container.Host(ctx)
-	require.NoError(t, err, "Failed to get the user service host")
-
-	port, err := container.MappedPort(ctx, "8080")
-	require.NoError(t, err, "Failed to get the user service port")
-
-	testUserService := TestUserService{
-		Host: host,
-		Port: port.Port(),
-	}
-
-	return &testUserService, cleanup
+	return cleanup
 }
