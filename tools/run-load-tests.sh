26b98f5889b2da748563d09f96a32701e8dc8f8f
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2025-06-14 12:49:41 +0200

    Add automatic dir creation for load test results

--- tools/run-load-tests.sh
+++ tools/run-load-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-OUTPUT_DIR="./report/report_$(date --iso-8601=seconds)"
+mkdir -p reports
+OUTPUT_DIR="./reports/report_$(date --iso-8601=seconds)"
 
 jmeter -n -t config/jmeter/load_test.jmx -l "$OUTPUT_DIR/result.jtl" -e -o "$OUTPUT_DIR"
