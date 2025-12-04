8a8ef1aaeda766ed35711c251c56fa8493893026
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2025-12-04 23:41:15 +0100

    Update comments for `ReverseGeocodeRequest`

--- commons/dtos/reverse_geocode_request.go
+++ commons/dtos/reverse_geocode_request.go
@@ -1,7 +1,6 @@
 package dtos
 
-// ReverseGeocodeRequest represents the request for reverse geocoding
 type ReverseGeocodeRequest struct {
-	Lat string `json:"lat"`
-	Lon string `json:"lon"`
+	Lat string `json:"lat" binding:"required" example:"40.7579554"`
+	Lon string `json:"lon" binding:"required" example:"-73.9855319"`
 }
